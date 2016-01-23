813fd1691b4ef633fbc06fe879b9f4c37822835f
Alexey Melezhik <melezhik@gmail.com>
2016-01-23 19:30:18 +0300

    add outth test

---
+++ test/integration/default/bash/outth_test.bash
@@ -0,0 +1,9 @@
+# here we go!
+PATH=$PATH:/usr/local/bin/
+sparrow 1>/dev/null || exit 1
+sparrow project create foo
+sparrow check add foo tele
+sparrow check set foo tele outth-telegraf
+sparrow check show foo tele 
+match_l=1000 sparrow check run foo tele
+
